1ba7c652ba400d9cb9571892175cae8678875dfc
Pascal Vizeli <pvizeli@syshack.ch>
2019-11-06 15:34:47 +0000

    fix lint

--- dude/homeassistant.py
+++ dude/homeassistant.py
@@ -1,6 +1,6 @@
 """Handle Home Assistant requests."""
 import os
-form typing import Dict, Mabye
+from typing import Dict, Maybe
 
 import requests
 
@@ -15,6 +15,11 @@ class HomeAssistant:
 
     def send_stt(self, data_gen) -> Dict[str, Maybe[str]]:
         """Send voice to STT handler."""
-        headers = {**self.headers, "X-Speech-Content": "format=wav; codec=pcm; sample_rate=16000; bit_rate=16; channel=1; language=en-US"}
-        req = requests.post(f"{self.url}/stt/cloud", data=data_gen, headers=headers, stream=True)
+        headers = {
+            **self.header,
+            "X-Speech-Content": "format=wav; codec=pcm; sample_rate=16000; bit_rate=16; channel=1; language=en-US",
+        }
+        req = requests.post(
+            f"{self.url}/stt/cloud", data=data_gen, headers=headers, stream=True
+        )
         return req.json()
